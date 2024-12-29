cc483f22dd48997b653f46314c060cfa3ff8219c
kapitu <ketkap007@gmail.com>
2024-12-29 21:56:55 +0200

    fix-generics

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,7 +1,8 @@
 
-  function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
     return Object.assign({}, objA, objB);
   }
+  
   
   const obj1 = { name: 'Alice' };
   const obj2 = { age: 25 };
